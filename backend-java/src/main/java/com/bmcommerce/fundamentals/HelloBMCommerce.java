8bdf94939c2d02e0c606003d5faa2e549f54b5c2
Bhagyashree Maharana <bhagyashree@Bhagyashrees-MacBook-Air.local>
2026-07-06 16:26:56 +0900

    feat : add first Java fundamentals program

---
+++ backend-java/src/main/java/com/bmcommerce/fundamentals/HelloBMCommerce.java
@@ -0,0 +1,11 @@
+package com.bmcommerce.fundamentals;
+
+public class HelloBMCommerce {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to  BM Commerce");
+        System.out.println("Java version : "+ System.getProperty("java.version"));
+        System.out.println("Operating system : "+System.getProperty("os.name"));
+        System.out.println("Current user : "+System.getProperty("user.name"));
+    }
+}
